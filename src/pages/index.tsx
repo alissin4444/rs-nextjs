9c42eea3b3c093046718dd137b4b727baa6f8ccf
alissin4444 <dev.alissonsantos@gmail.com>
2020-11-15 18:39:54 -0300

    basic client fetching using fetch

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,22 @@
+import { useState, useEffect } from "react";
+
 import { Title } from "../styles/pages/Home";
 
 export default function Home() {
+  const [recommendedProducts, setRecommendedProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3333/recommended").then((response) => {
+      response.json().then((data) => setRecommendedProducts(data));
+    });
+  }, []);
+
   return (
     <div>
       <Title>Hello World</Title>
+      {recommendedProducts.map((product) => (
+        <p key={product.id}>{product.title}</p>
+      ))}
     </div>
   );
 }
